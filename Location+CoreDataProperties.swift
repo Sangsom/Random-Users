7064f44f7ce3594e8a94b2b7143265cef6e594f6
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-12-27 12:25:18 +0200

    chore: Regenerating CoreData models

---
+++ Location+CoreDataProperties.swift
@@ -0,0 +1,32 @@
+//
+//  Location+CoreDataProperties.swift
+//  Random Users
+//
+//  Created by Rinalds Domanovs on 27/12/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Location {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Location> {
+        return NSFetchRequest<Location>(entityName: "Location")
+    }
+
+    @NSManaged public var city: String?
+    @NSManaged public var country: String?
+    @NSManaged public var latitude: String?
+    @NSManaged public var longitude: String?
+    @NSManaged public var postcode: String?
+    @NSManaged public var state: String?
+    @NSManaged public var streetName: String?
+    @NSManaged public var streetNumber: Int16
+    @NSManaged public var timezoneDescription: String?
+    @NSManaged public var timezoneOffset: String?
+    @NSManaged public var person: Person?
+
+}
---
+++ Person+CoreDataClass.swift
@@ -0,0 +1,20 @@
+//
+//  Person+CoreDataClass.swift
+//  Random Users
+//
+//  Created by Rinalds Domanovs on 27/12/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+    var fullName: String {
+        return "Rino \(firstName!) \(lastName!)"
+
+    }
+
+}
