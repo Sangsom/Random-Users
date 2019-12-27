2a597455c0c1f2a54d23a64e87ae3373407dc62c
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-12-27 13:50:19 +0200

    refactor: Using tableview instead of view controller for showing user details

--- Random Users/Views/UsersTableViewController.swift
+++ Random Users/Views/UsersTableViewController.swift
@@ -85,7 +85,7 @@ extension UsersTableViewController {
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "UserDetails") as? UserDetailsViewController {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "UserDetails") as? UserDetailsTableViewController {
             // Pass properties
             vc.user = people[indexPath.row]
             navigationController?.pushViewController(vc, animated: true)
