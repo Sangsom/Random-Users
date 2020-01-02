c60b8a7a543681220bf0af6267d8832edd545387
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-01-02 14:51:19 +0200

    feat: Setting up mapView in UserLocationVC

--- Random Users/Views/UserLocationViewController.swift
+++ Random Users/Views/UserLocationViewController.swift
@@ -7,16 +7,40 @@
 //
 
 import UIKit
+import MapKit
 
 class UserLocationViewController: UIViewController {
 
+    // MARK: - Properties
+    var mapView: MKMapView!
+
+    // MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        print("Showing user location")
-        self.view.backgroundColor = .red
-        
+        setupUI()
     }
 
+    // MARK: - Custom methods
+    func setupUI() {
+        self.view.backgroundColor = .white
+        title = "Rino"
+
+        setupMapView()
+    }
+
+    func setupMapView() {
+        mapView = MKMapView()
+          mapView.translatesAutoresizingMaskIntoConstraints = false
+          view.addSubview(mapView)
+
+          let constraints = [
+              mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+              mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+              mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+              mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+          ]
+
+        NSLayoutConstraint.activate(constraints)
+    }
 }
