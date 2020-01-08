01afb5d2509da145a87d488b2da0ffc89f285e8c
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-01-08 12:30:14 +0200

    fix: Removing print statement from PersistanceService controller

--- Random Users/Controllers/PersistanceService.swift
+++ Random Users/Controllers/PersistanceService.swift
@@ -53,7 +53,6 @@ class PersistanceService {
         if context.hasChanges {
             do {
                 try context.save()
-                print("Saved")
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -62,5 +61,4 @@ class PersistanceService {
             }
         }
     }
-
 }
